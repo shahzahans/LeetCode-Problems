80a3392d9a72265a654c39f74b41b73bf7af5829
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-08-23 23:51:35 -0700

    Update Minimum Swapped.cpp

--- Jp-Morgan Practice/Minimum Swapped.cpp
+++ Jp-Morgan Practice/Minimum Swapped.cpp
@@ -1,8 +1,8 @@
 /**
  * LeetCode Practice Problem 
  * ~ Shadman S.
- * Problem Statement:You are given two strings s1 and s2 of equal 
- * length consisting of letters "x" and "y" only.Your task is to make these two 
+ * Problem Statement: You are given two strings s1 and s2 of equal 
+ * length consisting of letters "x" and "y" only. Your task is to make these two 
  * strings equal to each other. You can swap any two characters that belong to 
  * different strings, which means: swap s1[i] and s2[j].
  * Return the minimum number of swaps required to make s1 and s2 equal, 
@@ -15,9 +15,9 @@ using namespace std;
 
 int minSwap (string s1, string s2){
     
-    int x = 0;
-    int y = 0;
-    for (int i=0; i < s1.size(); i++){
+    int x = 0; // integer variable to keep track 
+    int y = 0; // integer variable to keep track
+    for (int i=0; i < s1.size(); i++){ // loop to find similarities and count them
         if (s1[i] == 'x' && s2[i] == 'y'){
             x++;
         }
@@ -26,11 +26,11 @@ int minSwap (string s1, string s2){
         }
     }
     
-    if ( (x+y)%2 != 0){
+    if ( (x+y)%2 != 0){ // if the remainder is not zero then return -1
         return -1;
     }
     
-    return (x/2) + (y/2) + (x%2) * 2;
+    return (x/2) + (y/2) + (x%2) * 2; 
 }
 
 int main()
