753aa65c45dc87e05304e03437747c4b30c6f234
Shadman Shahzahan <145942385+shahzahans@users.noreply.github.com>
2024-08-24 23:50:05 -0700

    Add files via upload

---
+++ Jp-Morgan Practice/Happy Number.cpp
@@ -0,0 +1,47 @@
+/**
+ * LeetCode Practice Problem 
+ * ~ Shadman S.
+ * Problem Statement: Write an algorithm to determine if a number n is happy.
+ * A happy number is a number defined by the following process:
+ * - Starting with any positive integer, replace the number by the sum of the squares 
+ *   of its digits.
+ * - Repeat the process until the number equals 1 (where it will stay), or it 
+ *   loops endlessly in a cycle which does not include 1.
+ * - Those numbers for which this process ends in 1 are happy.
+ */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool isHappy(int n){
+    unordered_set <int> seen;
+    
+    while (n != 1 && seen.find(n) == seen.end()){
+        seen.insert(n);
+        
+        int sum = 0;
+        
+        while (n!=0){
+            int digit = n %10;
+            sum += digit * digit;
+            n/=10;
+        }
+        n = sum;
+    }
+    return n == 1;
+}
+
+int main()
+{
+    int n;
+    
+    cout << "Enter a number to see if it reaches a happy number (1)" << endl;
+    
+    cin >> n;
+    
+    cout << isHappy(n);
+
+    return 0;
+}
